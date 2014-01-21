3e359d7b024b7e7a88635e4d9b306d0b77b2f31e
Karen Yin-Yee Ng <karen.yyng@gmail.com>
2014-01-20 18:32:23 -0800

    got wcs_ICRS working as intended for doing conversion from world to pix

--- wcs_ICRS.py
+++ wcs_ICRS.py
@@ -3,7 +3,7 @@ Author: Karen Ng <karenyng@ucdavis.edu>
 License: BSD 3-clause
 Purpose:
 """
-from __future__ import(division, print_function, unicode_literals)
+from __future__ import(division, unicode_literals)
 import numpy as np
 import astropy as ap
 from astropy.io import fits
@@ -32,13 +32,39 @@ class wcs_ICRS(ap.coordinates.builtin_systems.ICRS,
         ==========
         See documentation from superclass
 
+        Optional parameters:
+        ===================
+        fitsname = string
+            denoting path to fits file containing wcs info
+        wcs = astropy.wcs.wcs object
+            denote the wcs info
+
         Methods:
         =======
         set_wcs
-
         """
+        # kluegy way of incorporting optional parameters
+        if kwargs.has_key('fitsname'):
+            self.fitsname = kwargs.get('fitsname', None)
+            del kwargs['fitsname']
+
+        if kwargs.has_key('wcs'):
+            self.wcs = kwargs.get('wcs', None)
+            del kwargs['wcs']
+
+        self.verbose = False
+        if kwargs.has_key('verbose'):
+            self.verbose = kwargs.get('verbose', False)
+            del kwargs['verbose']
+
+
         ICRS.__init__(self, *args, **kwargs)
-        #print self.ra
+
+        # kluegy way of incorporting optional parameters
+        if hasattr(self, 'wcs'):
+            self.set_wcs(local_wcs=self.wcs, verbose=self.verbose)
+        if hasattr(self, 'fitsname'):
+            self.set_wcs(fitsname=self.fitsname, verbose=self.verbose)
 
 
     def get_WCS_from_fits(self, fitsname, verbose=False):
@@ -50,27 +76,29 @@ class wcs_ICRS(ap.coordinates.builtin_systems.ICRS,
         hdulist = fits.open(fitsname)
         w = wcs.WCS(hdulist[0].header)
         if verbose:
+            print "successfully loaded wcs from fits"
             w.wcs.print_contents()
         return w
 
 
-    def set_wcs(self, wcs=None, fitsname=None):
+    def set_wcs(self, fitsname=None, local_wcs=None, verbose=False):
         """set
         Parameters:
         ==========
 
         """
         if fitsname is not None:
-            self.wcs = self.get_WCS_from_fits(fitsname)
-        elif wcs is not None:
-            # store wcs info
-            assert type(wcs) == ap.wcs.wcs.WCS, "print not " + \
-                " of right  type,\ntype(wcs) needs to be " + \
-                "astropy.wcs.wcs.WCS"
-            self.wcs = wcs
+            self.wcs = self.get_WCS_from_fits(fitsname, verbose)
         else:
-            raise ValueError("input of fitsname and wcs are both " +
-                             "None - one of them has to be valid")
+            if local_wcs is not None:
+                # store wcs info
+                assert type(local_wcs) == ap.wcs.wcs.WCS, "print not " + \
+                    " of right  type,\ntype(wcs) needs to be " + \
+                    "astropy.wcs.wcs.WCS"
+                self.wcs = local_wcs
+            else:
+                raise ValueError("input of fitsname and wcs are both " +
+                                "None - one of them has to be valid")
 
 
         # kluegy way of doing transformation
@@ -80,6 +108,5 @@ class wcs_ICRS(ap.coordinates.builtin_systems.ICRS,
               super(wcs_ICRS, self).dec.value],
              [super(wcs_ICRS, self).ra.value,
               super(wcs_ICRS, self).dec.value]]),1)
-
-
-
+        if verbose:
+            print "set self.pixcoord to {0}".format(self.pixcoord)
